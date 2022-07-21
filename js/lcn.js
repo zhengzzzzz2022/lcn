4d512418bd41e4105e9d076af8003292b14f60cc
zhengzzzzz2022 <109058610+zhengzzzzz2022@users.noreply.github.com>
2022-07-21 16:20:40 +0800

    Update lcn.js

--- js/lcn.js
+++ js/lcn.js
@@ -9,24 +9,3 @@ myImage.onclick = function () {
   }
 };
 
-let myButton = document.querySelector("button");
-let myHeading = document.querySelector("h1");
-function setUserName() {
-  let myName = prompt("请输入你的名字");
-  if (!myName) {
-    setUserName();
-  } else {
-    localStorage.setItem("name", myName);
-    myHeading.textContent = "好的" + myName;
-  }
-}
-if (!localStorage.getItem("name")) {
-  setUserName();
-} else {
-  let storeName = localStorage.getItem("name");
-  myHeading.textContent = "好样的" + storeName;
-}
-
-myButton.onclick = function () {
-  setUserName();
-};
