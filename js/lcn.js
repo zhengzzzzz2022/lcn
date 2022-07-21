2f6ff0c9088a88e1e2723a38db76a6a5b7b7d961
zhengzzzzz2022 <109058610+zhengzzzzz2022@users.noreply.github.com>
2022-07-21 16:24:13 +0800

    Update lcn.js

--- js/lcn.js
+++ js/lcn.js
@@ -1,10 +1,10 @@
 let myImage = document.querySelector("img");
 myImage.onclick = function () {
   let mySrc = myImage.getAttribute("src");
-  if (mySrc === "../img/card_bg_blue.png") {
-    myImage.setAttribute("src", "../img/card_bg_yellow.png");
+  if (mySrc === "img/card_bg_blue.png") {
+    myImage.setAttribute("src", "img/card_bg_yellow.png");
   } else {
-    myImage.setAttribute("src", "../img/card_bg_blue.png");
+    myImage.setAttribute("src", "img/card_bg_blue.png");
     console.log(mySrc);
   }
 };
